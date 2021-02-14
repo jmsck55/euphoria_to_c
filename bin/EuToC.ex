6aa1d44111a3384d2b6b384f20baa1e40fe8a6d1
James J. Cook <jmsck55@gmail.com>
2021-02-14 06:38:06 -0500

    Create EuToC.ex
    Template file for now, but missing Copyright statement.

---
+++ bin/EuToC.ex
@@ -0,0 +1,19 @@
+-- EuToC.ex translates Euphoria code into pure C code that is architecture independent.
+-- Copyright (c) 2021 James Cook
+
+-- CONSTRUCTION: Put Copyright statement here.
+
+-- You can contact me online at: jmsck55 <at> gmail <dot> com
+-- Please put something intelligent in the subject line so that I will know that it is important.
+
+-- You can also send snail mail to me at:
+--  James J Cook
+--  14 Glen Ln.
+--  Randolph, MA 02368
+--  USA
+
+--Begin code, in Euphoria v4.0.5
+
+-- CONSTRUCTION: Put body of code here.
+
+--end of file.
